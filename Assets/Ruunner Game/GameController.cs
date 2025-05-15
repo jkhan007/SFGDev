6c0ac9f69cd84381e665ab56a144e3ceaae5cb53
jawadkhan1551 <91638772+jawadkhan1551@users.noreply.github.com>
2025-05-15 11:37:25 +0500

    New Assets Added

---
+++ Assets/Ruunner Game/GameController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject envPrefab;
+
+
+    void SpawnNewEnv()
+    {
+     
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
